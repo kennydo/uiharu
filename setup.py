cb6a78bbd3151f6a42b44d2440dcbd23d8da34a1
Kenny Do <chinesedewey@gmail.com>
2015-09-13 13:21:30 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='uiharu',
+    version='0.0.1',
+    description='Python temperature sensor project',
+    long_description='',
+    url='https://github.com/kennydo/uiharu',
+    author='Kenny Do',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+    ],
+    keywords='temperature sensor',
+    packages=find_packages(),
+    install_requires=[
+        'alembic==0.8.2',
+        'pyusb==1.0.0b1',
+        'SQLAlchemy==1.0.8',
+        'temperusb==1.2.3',
+    ],
+    tests_require=[
+    ],
+    entry_points={
+    },
+)
