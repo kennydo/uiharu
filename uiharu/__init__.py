805e67ad540e3072929dea30b8894af87fc622ef
Kenny Do <chinesedewey@gmail.com>
2016-08-13 18:37:27 -0700

    Remove flask usage in init

--- uiharu/__init__.py
+++ uiharu/__init__.py
@@ -1,21 +1,5 @@
 import logging
 
-from flask import Flask
-
 
 log = logging.getLogger(__name__)
 
-
-def create_app(config_dict):
-    app = Flask(__name__, static_folder=None)
-    app.config.update(**config_dict)
-
-    from uiharu.api.views import api as api_blueprint
-    from uiharu.weather.views import weather as weather_blueprint
-
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
-    app.register_blueprint(weather_blueprint)
-
-    log.info(app.url_map)
-
-    return app
